ab9cedceaee2619d6014bcec8b72009e1355460c
Wei Kang <chongweikang5@gmail.com>
2023-03-03 17:02:11 +0800

    added asset images and partial folders

--- IQ_Main/settings.py
+++ IQ_Main/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Root folder to store collected static files
+STATIC_ROOT = BASE_DIR/'static'
+
+# All the static file directories in the project.
+STATICFILES_DIRS = [
+    BASE_DIR/'IQ_Main/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
