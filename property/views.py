d7c3c78f664720359d840734351e883a44f97a95
Wei Kang <chongweikang5@gmail.com>
2023-03-12 20:13:22 +0800

    merge

--- property/views.py
+++ property/views.py
@@ -1,23 +1,17 @@
+import os
+from ura_api import ura_api
+from dotenv import load_dotenv
+load_dotenv()
 from django.shortcuts import render
 from .models import Property
+from .models import Property
 # Create your views here.
-import os
-from dotenv import load_dotenv
-from ura_api import ura_api
-load_dotenv()
 
-def saveDataToDatabase():
+def savePropertyToDatabase():
     URA_API_KEY = os.getenv('URA_API_KEY')
 
     ura = ura_api.ura_api(URA_API_KEY)
     data = ura.private_residential_properties_rental_contract('23q1')
-    # print(data) # will print out the whole data.
-    print(type(data[0]))  # type list
-    print()
-    print(len(data)) # 1886 rows in 2023 q1
-    print()
-    print(data[0]) # first element
-    print()
 
     property_list = []
 
@@ -62,3 +56,5 @@ def index(request):
 
 def property(request):
     pass
+
+savePropertyToDatabase()
