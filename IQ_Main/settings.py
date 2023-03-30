c599f5638db80f58224bb5fb2a9051ee5c749c6c
Wei Kang <chongweikang5@gmail.com>
2023-03-30 13:13:37 +0800

    implemented validators for passwords

--- IQ_Main/settings.py
+++ IQ_Main/settings.py
@@ -107,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+            'OPTIONS':{
+                'min_length': 3,
+            }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
